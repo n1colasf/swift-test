c861f6ed2f75adc859e30b7b0eaf348215179f8b
Nicolas <nicolas@nicolasf.uy>
2024-10-11 20:47:05 -0300

    Initial Commit

---
+++ swift-test/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  swift-test
+//
+//  Created by Nicolas Fernandez on 11/10/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ swift-test/swift_testApp.swift
@@ -0,0 +1,32 @@
+//
+//  swift_testApp.swift
+//  swift-test
+//
+//  Created by Nicolas Fernandez on 11/10/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct swift_testApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
